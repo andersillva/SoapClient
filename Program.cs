be849d46bed05a663678e9632c39fa3fdb5d08ef
Anderson Machado da Silva <silva.andersonm@gmail.com>
2022-02-20 21:19:51 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace SoapClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            SoapDemo.SOAPDemoSoapClient client = new();
+
+            long sum = await client.AddIntegerAsync(1, 2);
+            Console.WriteLine($"Soma: {sum}");
+
+            var response = await client.GetListByNameAsync("Adams");
+            Console.WriteLine($"Registros encontrados: {response[0].Name}");
+
+            var city = await client.LookupCityAsync("99950");
+            Console.WriteLine($"Cidade: {city.City}");
+        }
+    }
+}
